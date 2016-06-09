2de88f83e079ccb3d8dfa5a491b3ff332ad80d8e
DIALLO <Ibtra@MacBook-Pro-de-DIALLO.local>
2016-06-09 12:26:54 +0200

    Added the form to add an analysis

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: 'home#index'
+  root to: 'video_games#index'
 
   resources :video_games, only: [:index, :show, :create, :new, :edit]
-  resources :artists, only: [:index, :show]
+  resources :analysis, only: [:index, :show, :create, :new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
